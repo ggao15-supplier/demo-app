f9c8b41ef4dfa5617f5b2c7d802c27f682d26b85
ggao15-ford <ggao15@ford.com>
2020-03-21 21:21:58 +0800

    使用自定义drawable 实现XFModel 圆形图片

--- app/src/main/java/com/e/myapplication/MainActivity.kt
+++ app/src/main/java/com/e/myapplication/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.e.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.view.menu.MenuBuilder
+import com.e.roundhead.RoundImageLoad
+import com.e.roundhead.customdrawable.RoundDrawable
+import com.e.roundhead.xfmodel.XFModelRoundImageLoad
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolBar)
+        ivHead.setImageDrawable(
+            RoundDrawable(
+                BitmapFactory.decodeResource(
+                    resources,
+                    R.mipmap.ic_launcher
+                )
+            )
+        )
+
     }
 }
---
+++ roundhead/src/main/java/com/e/roundhead/customdrawable/RoundDrawable.kt
@@ -0,0 +1,44 @@
+package com.e.roundhead.customdrawable
+
+import android.graphics.*
+import android.graphics.drawable.Drawable
+
+class RoundDrawable(private val srcBitmap: Bitmap) : Drawable() {
+    private val paint = Paint()
+    override fun draw(canvas: Canvas) {
+        val width = srcBitmap.width
+        val height = srcBitmap.height
+        canvas.drawColor(Color.TRANSPARENT)
+        var layerId = canvas.saveLayer(
+            0f,
+            0f,
+            canvas.width.toFloat(),
+            canvas.height.toFloat(),
+            null,
+            Canvas.ALL_SAVE_FLAG
+        )
+        paint.color = Color.WHITE
+        val radius: Float = if (width > height) (height / 2).toFloat() else (width / 2).toFloat()
+        canvas.drawCircle(radius, radius, radius-20, paint)
+
+        paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
+        canvas.drawBitmap(srcBitmap, 0f, 0f, paint)
+        paint.xfermode = null
+        canvas.restoreToCount(layerId)
+    }
+
+    override fun setAlpha(alpha: Int) {
+        paint.alpha = alpha
+        invalidateSelf()
+    }
+
+    override fun getOpacity(): Int {
+        return PixelFormat.TRANSLUCENT
+    }
+
+    override fun setColorFilter(colorFilter: ColorFilter?) {
+        paint.colorFilter = colorFilter
+        invalidateSelf()
+    }
+
+}
