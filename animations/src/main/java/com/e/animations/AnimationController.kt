534dc4bf0523d87b3e22b0642e0f54c9d52e4322
ggao15-ford <ggao15@ford.com>
2020-03-23 12:42:39 +0800

    使用view wrapper 实现view的object animator 和使用ValueAnimator 实现 不可以定义setter和getter的view 属性

--- animations/src/main/java/com/e/animations/AnimationController.kt
+++ animations/src/main/java/com/e/animations/AnimationController.kt
@@ -1,7 +1,13 @@
 package com.e.animations
 
+import android.animation.Animator
+import android.animation.IntEvaluator
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
+import android.view.animation.AccelerateDecelerateInterpolator
 import android.view.animation.AnimationUtils
 import android.view.animation.LayoutAnimationController
+import android.widget.LinearLayout
 import androidx.recyclerview.widget.RecyclerView
 
 class AnimationController {
@@ -14,5 +20,59 @@ class AnimationController {
             layoutAnimationController.order = LayoutAnimationController.ORDER_NORMAL
             listView.layoutAnimation = layoutAnimationController
         }
+
+        fun resetLinearLayoutWidthWithWrapper(
+            target: LinearLayoutSizeWrapper,
+            width: Int,
+            listener: OnResetFinishListener
+        ) {
+            val objectAnimator = ObjectAnimator.ofInt(target, "width", width).setDuration(500)
+            objectAnimator.addListener(object : Animator.AnimatorListener {
+                override fun onAnimationRepeat(animation: Animator?) {}
+
+                override fun onAnimationEnd(animation: Animator?) {
+                    listener.onRestFinish()
+                }
+
+                override fun onAnimationCancel(animation: Animator?) {}
+
+                override fun onAnimationStart(animation: Animator?) {}
+
+            })
+            objectAnimator.setEvaluator(IntEvaluator())
+            objectAnimator.interpolator = AccelerateDecelerateInterpolator()
+            objectAnimator.start()
+        }
+
+        fun resetLinearLayoutWidthWithValue(
+            target: LinearLayout,
+            width: Int,
+            listener: OnResetFinishListener
+        ) {
+            val valueAnimator = ValueAnimator.ofInt(width)
+            valueAnimator.addUpdateListener {
+                target.layoutParams.width = it.animatedValue as Int
+                target.requestLayout()
+            }
+            valueAnimator.addListener(object : Animator.AnimatorListener {
+                override fun onAnimationRepeat(animation: Animator?) {}
+
+                override fun onAnimationEnd(animation: Animator?) {
+                    listener.onRestFinish()
+                }
+
+                override fun onAnimationCancel(animation: Animator?) {}
+
+                override fun onAnimationStart(animation: Animator?) {}
+
+            })
+            valueAnimator.setEvaluator(IntEvaluator())
+            valueAnimator.interpolator = AccelerateDecelerateInterpolator()
+            valueAnimator.start()
+        }
+    }
+
+    interface OnResetFinishListener {
+        fun onRestFinish()
     }
 }
