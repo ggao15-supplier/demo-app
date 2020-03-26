bc1ad96eb376a3e47955e594dc97067ee9280fa5
ggao15-ford <ggao15@ford.com>
2020-03-26 16:43:15 +0800

    使用 window manager 添加 button

--- windowmanagers/src/main/java/com/fprdmps/mobileappcn/windowmanagers/FloatButtonWindowManager.kt
+++ windowmanagers/src/main/java/com/fprdmps/mobileappcn/windowmanagers/FloatButtonWindowManager.kt
@@ -5,51 +5,49 @@ import android.graphics.Color
 import android.graphics.PixelFormat
 import android.util.Log
 import android.view.Gravity
-import android.view.MotionEvent
-import android.view.View
 import android.view.WindowManager
 import android.view.WindowManager.LayoutParams.*
-import android.widget.ImageButton
+import androidx.core.content.ContextCompat
 
 class FloatButtonWindowManager(
     private val windowManager: WindowManager,
-    context: Context
+    private val context: Context
 ) {
     private val layoutParams: WindowManager.LayoutParams = WindowManager.LayoutParams(
-        WRAP_CONTENT,
-        WRAP_CONTENT,
+        200,
+        200,
         TYPE_APPLICATION,
         FLAG_NOT_FOCUSABLE,
-        PixelFormat.TRANSPARENT
+        PixelFormat.RGBA_8888
     )
-    private val button = ImageButton(context)
+    private val button = FloatButton(context)
 
     init {
         addView()
+
     }
 
     private fun addView() {
         button.setPadding(50, 50, 50, 50)
-        button.setBackgroundColor(Color.BLUE)
-        button.setImageResource(android.R.drawable.stat_notify_sync_noanim)
-        layoutParams.gravity = Gravity.CENTER or Gravity.END
+        button.setBackgroundColor(ContextCompat.getColor(context, android.R.color.holo_green_dark))
+        button.setImageResource(android.R.drawable.button_onoff_indicator_off)
+        //必须设置为这个，否则会出现 touch 事件中 rawX/rawY为整个屏幕的宽高问题
+        layoutParams.gravity = Gravity.TOP or Gravity.START
+        layoutParams.x = 400
+        layoutParams.y = 400
         windowManager.addView(button, layoutParams)
-        button.setOnTouchListener(initTouchListener())
+        button.listener = object : OnUpdateLayoutListener {
+            override fun onUpdateLayout(x: Int, y: Int) {
+                Log.d("xxx", "x:$x;;y:$y")
+                layoutParams.x = x
+                layoutParams.y = y
+                windowManager.updateViewLayout(button, layoutParams)
+            }
+
+        }
     }
 
-    private fun initTouchListener(): View.OnTouchListener {
-        return View.OnTouchListener { _, event ->
-            when (event.action) {
-                MotionEvent.ACTION_MOVE -> {
-                    val x = event.rawX
-                    val y = event.rawY
-                    Log.d("xxx","x:$x;;y:$y")
-                    layoutParams.x = x.toInt()
-                    layoutParams.y = y.toInt()
-                    windowManager.updateViewLayout(button, layoutParams)
-                }
-            }
-            return@OnTouchListener false
-        }
+    interface OnUpdateLayoutListener {
+        fun onUpdateLayout(x: Int, y: Int)
     }
 }
