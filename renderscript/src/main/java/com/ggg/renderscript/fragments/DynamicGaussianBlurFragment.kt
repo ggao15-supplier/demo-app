724f3c03b859e8b50984d89a69504940c7d1859d
ggao15-supplier <g1195643514@163.com>
2021-05-15 11:15:58 +0800

    modify dynamic gaussian show view

--- renderscript/src/main/java/com/ggg/renderscript/fragments/DynamicGaussianBlurFragment.kt
+++ renderscript/src/main/java/com/ggg/renderscript/fragments/DynamicGaussianBlurFragment.kt
@@ -1,12 +1,9 @@
 package com.ggg.renderscript.fragments
 
-import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.graphics.Paint
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
-import android.view.SurfaceHolder
 import android.view.View
 import android.view.ViewGroup
 import android.widget.SeekBar
@@ -17,7 +14,6 @@ import androidx.renderscript.RenderScript
 import androidx.renderscript.ScriptIntrinsicBlur
 import com.ggg.renderscript.R
 import com.ggg.renderscript.databinding.FragmentDynamicGaussianBlurBinding
-import com.ggg.renderscript.databinding.FragmentGaussianBlurBinding
 
 
 /**
@@ -25,40 +21,15 @@ import com.ggg.renderscript.databinding.FragmentGaussianBlurBinding
  */
 class DynamicGaussianBlurFragment : Fragment() {
     private var bind: FragmentDynamicGaussianBlurBinding? = null
-    private var holderPreview: SurfaceHolder? = null
-    private val paint = Paint()
-    private var previewWidth: Float = 0f
-    private var previewHeight: Float = 0f
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        paint.isAntiAlias = true
-        paint.style = Paint.Style.STROKE
+
         bind = FragmentDynamicGaussianBlurBinding.inflate(inflater, container, false)
-        bind?.sfOriginal?.holder?.addCallback(object : SurfaceHolder.Callback {
-            override fun surfaceChanged(
-                holder: SurfaceHolder,
-                format: Int,
-                width: Int,
-                height: Int
-            ) {
-                previewHeight = height.toFloat()
-                previewWidth = width.toFloat()
-                holderPreview = holder
-
-                draw(BitmapFactory.decodeResource(resources, R.mipmap.test_blur))
-
-            }
-
-            override fun surfaceDestroyed(holder: SurfaceHolder) {
-            }
-
-            override fun surfaceCreated(holder: SurfaceHolder) {
-            }
-
-        })
+        bind?.ivOriginal?.setImageResource(R.mipmap.test_blur)
         bind?.seekBar?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 turn(progress.toFloat() / 100.0f)
@@ -74,14 +45,6 @@ class DynamicGaussianBlurFragment : Fragment() {
         return bind?.root
     }
 
-    private fun draw(bitmap: Bitmap) {
-        holderPreview?.apply {
-            val canvas = lockCanvas()
-            canvas.drawBitmap(bitmap, 0f, 0f, paint)
-            unlockCanvasAndPost(canvas)
-        }
-    }
-
     private fun turn(radius: Float) {
         Log.d("xxx", "radius:$radius")
         val rs: RenderScript = RenderScript.create(context)
@@ -95,7 +58,7 @@ class DynamicGaussianBlurFragment : Fragment() {
         blurScript.forEach(blurAllocation)
 
         blurAllocation.copyTo(bitmap)
-        draw(bitmap)
+        bind?.ivOriginal?.setImageBitmap(bitmap)
         allocation.destroy()
         blurAllocation.destroy()
         blurScript.destroy()
