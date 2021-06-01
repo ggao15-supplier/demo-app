630c1ed4c7dfa97356ef601288c090347bbce5ac
gggao <gggao@telenav.cn>
2021-06-01 10:36:43 +0800

    modify jni

--- jniutils/src/main/cpp/nativelib.cpp
+++ jniutils/src/main/cpp/nativelib.cpp
@@ -1,12 +1,15 @@
 //
 // Created by gggao on 5/19/2021.
+//https://juejin.cn/post/6844904192780271630
 //
 
 #include <jni.h>
 #include <string>
+#include <cstdlib>
 
-extern "C" jstring JNICALL Java_com_ggg_jniutils_jni_JNIUtils_callMD5
-  (JNIEnv *env, jobject obj, jstring data){
-
-return env->NewStringUTF("hello world");
+extern "C" JNIEXPORT jstring JNICALL Java_com_ggg_jniutils_jni_JNIUtils_callMD5
+        (JNIEnv *env, jobject obj, jstring data) {
+    const char *c_str = env->GetStringUTFChars(data, nullptr);
+    jclass jclass1 = env->GetObjectClass(obj);
+    return env->NewStringUTF(c_str);
 }
