5f95a320126e47dc99cb7a097f25ae0991cb8a4b
gggao <gggao@telenav.cn>
2021-06-02 10:17:43 +0800

    add jni log

--- jniutils/src/main/cpp/nativelib.cpp
+++ jniutils/src/main/cpp/nativelib.cpp
@@ -6,10 +6,18 @@
 #include <jni.h>
 #include <string>
 #include <cstdlib>
+#include <android/log.h>
+
+#define  LOGE(...) __android_log_print(ANDROID_LOG_ERROR,"JNI",__VA_ARGS__)
 
 extern "C" JNIEXPORT jstring JNICALL Java_com_ggg_jniutils_jni_JNIUtils_callMD5
         (JNIEnv *env, jobject obj, jstring data) {
     const char *c_str = env->GetStringUTFChars(data, nullptr);
-    jclass jclass1 = env->GetObjectClass(obj);
+    jclass clazz = env->GetObjectClass(obj);
+    jmethodID method_getTypeValue = env->GetMethodID(clazz, "getTypeValue",
+                                                     "(I)Ljava/lang/String;");
+    auto values = reinterpret_cast<jstring>( env->CallObjectMethod(obj, method_getTypeValue,
+                                                                   1));
+    LOGE("value is %s", env->GetStringUTFChars(values, nullptr));
     return env->NewStringUTF(c_str);
 }
--- jniutils/src/main/java/com/ggg/jniutils/jni/JNIUtils.kt
+++ jniutils/src/main/java/com/ggg/jniutils/jni/JNIUtils.kt
@@ -4,8 +4,18 @@ package com.ggg.jniutils.jni
  * Created by  gggao on 5/21/2021.
  */
 class JNIUtils {
-   init {
-       System.loadLibrary("native-lib")
-   }
+    init {
+        System.loadLibrary("native-lib")
+    }
+
     external fun callMD5(value: String): String
+
+    fun getTypeValue(type: Int): String {
+        return when (type) {
+            1 -> "1 value is 1"
+            2 -> "1 value is 1"
+            3 -> "1 value is 1"
+            else -> "empty"
+        }
+    }
 }
